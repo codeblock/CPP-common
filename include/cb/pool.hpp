6b8b5ac6d052ac80542cc08dccacae92c187b51f
codeblock <beanfondue@gmail.com>
2022-06-15 02:09:18 +0900

    Code cleanup
    code format to K&R from BSD
    added Pool.m_timeout

--- include/cb/pool.hpp
+++ include/cb/pool.hpp
@@ -11,16 +11,14 @@
 
 namespace cb {
 
-class IPoolResource
-{
-public:
+class IPoolResource {
+ public:
   virtual bool connect() = 0;
   virtual bool disconnect() = 0;
 };
 
 template <typename T>
-struct tagPoolBody
-{
+struct tagPoolBody {
   T* body; // class T : public IPoolResource
   int timeout;
   time_t past;
@@ -28,9 +26,8 @@ struct tagPoolBody
 };
 
 template <typename T> // class T : public IPoolResource
-class Pool
-{
-public:
+class Pool {
+ public:
   Pool(void);
   ~Pool(void);
 
@@ -38,48 +35,43 @@ public:
   T* get(void);
   void release(T* rsc);
   void clear(void);
-private:
+ private:
   Pool(const Pool& rhw);
   Pool& operator =(const Pool& rhw);
   void* operator new (std::size_t) throw (std::bad_alloc); // prevent dynamic allocation
-private:
+ private:
   std::mutex m_mtx;
   int m_size;
   int m_wait;
+  int m_timeout;
   std::vector<tagPoolBody<T>> m_vec;
   std::hash<T*> m_hash;
 };
 
 template <typename T>
-Pool<T>::Pool(void) : m_size(0), m_wait(0)
-{
+Pool<T>::Pool(void) : m_size(0), m_wait(0), m_timeout(28800) {
 }
 
 template <typename T>
-Pool<T>::~Pool(void)
-{
+Pool<T>::~Pool(void) {
   //clear();
 }
 
 template <typename T>
-void Pool<T>::set(unsigned int n)
-{
+void Pool<T>::set(unsigned int n) {
   short s = 0;
 
-  if (m_size == 0)
-  {
+  if (m_size == 0) {
     m_mtx.lock();
-    if (m_size == 0)
-    {
+    if (m_size == 0) {
       m_size = n;
       m_wait = 1;//m_size << 1;
       m_vec.reserve(m_size);
-      for (s = 0; s < m_size; ++s)
-      {
+      for (s = 0; s < m_size; ++s) {
         tagPoolBody<T> ps;
         ps.body = new T();
         //ps.body->connect(); // not yet set the connection informations
-        ps.timeout = 28800 - 1;
+        ps.timeout = m_timeout;
         ps.past = time(NULL);
         ps.use = 0;
         m_vec.push_back(ps);
@@ -92,31 +84,24 @@ void Pool<T>::set(unsigned int n)
 }
 
 template <typename T>
-T* Pool<T>::get(void)
-{
+T* Pool<T>::get(void) {
   typename std::vector<tagPoolBody<T>>::iterator it = m_vec.begin();
 
   short s = 0;
   T* rsc = NULL;
   time_t past;
 
-  if (m_size > 0)
-  {
+  if (m_size > 0) {
     past = time(NULL);
-    while (rsc == NULL)
-    {
+    while (rsc == NULL) {
       s = 0;
       //printf("(%d / %d)\n", time(NULL) - past, m_wait);
-      for (auto it = m_vec.begin(); it != m_vec.end(); ++it)
-      {
+      for (auto it = m_vec.begin(); it != m_vec.end(); ++it) {
         s++;
-        if (it->use == 0)
-        {
+        if (it->use == 0) {
           m_mtx.lock();
-          if (it->use == 0)
-          {
-            if (time(NULL) - it->past > it->timeout)
-            {
+          if (it->use == 0) {
+            if (time(NULL) - it->past > it->timeout) {
               cb::Logger::log(cb::Logger::eLevel::eDebug, "%s%hd%s%d%s%d%s", "Pool refresh #", s, " for timeout (", (time(NULL) - it->past), " / ", it->timeout, " sec)");
               it->body->disconnect();
               it->body->connect();
@@ -129,8 +114,7 @@ T* Pool<T>::get(void)
           }
           m_mtx.unlock();
 
-          if (it->use == 1)
-          {
+          if (it->use == 1) {
             break;
           }
         }
@@ -152,19 +136,15 @@ T* Pool<T>::get(void)
 }
 
 template <typename T>
-void Pool<T>::release(T* rsc)
-{
+void Pool<T>::release(T* rsc) {
   typename std::vector<tagPoolBody<T>>::iterator it = m_vec.begin();
 
   short s = 0;
 
-  if (m_size > 0)
-  {
-    for (auto it = m_vec.begin(); it != m_vec.end(); ++it)
-    {
+  if (m_size > 0) {
+    for (auto it = m_vec.begin(); it != m_vec.end(); ++it) {
       s++;
-      if (it->use == 1 && m_hash(it->body) == m_hash(rsc))
-      {
+      if (it->use == 1 && m_hash(it->body) == m_hash(rsc)) {
         it->use = 0;
 
         cb::Logger::log(cb::Logger::eLevel::eInfo, "%s%d", "Pool release #", s);
@@ -175,19 +155,15 @@ void Pool<T>::release(T* rsc)
 }
 
 template <typename T>
-void Pool<T>::clear(void)
-{
+void Pool<T>::clear(void) {
   typename std::vector<tagPoolBody<T>>::iterator it = m_vec.begin();
 
   short s = 0;
 
-  if (m_size > 0)
-  {
+  if (m_size > 0) {
     m_mtx.lock();
-    if (m_size > 0)
-    {
-      for (auto it = m_vec.begin(); it != m_vec.end(); ++it)
-      {
+    if (m_size > 0) {
+      for (auto it = m_vec.begin(); it != m_vec.end(); ++it) {
         s++;
         it->body->disconnect();
         delete it->body; // warning: deleting object of polymorphic class type ‘PoolMySQL’ which has non-virtual destructor might cause undefined behaviour [-Wdelete-non-virtual-dtor]
