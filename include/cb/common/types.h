f897418d63b76cea2d2ea3a0f107bc9a3d426ce9
codeblock <beanfondue@gmail.com>
2022-06-28 21:40:11 +0900

    types container

---
+++ include/cb/common/types.h
@@ -0,0 +1,22 @@
+#ifndef CB_COMMON_TYPES_H_
+#define CB_COMMON_TYPES_H_
+
+#include <unordered_map>
+
+namespace cb {
+namespace common {
+namespace types {
+
+typedef struct {
+  std::string method;
+  std::string path;
+  std::string remote_addr;
+  unsigned short remote_port;
+  std::unordered_map<std::string, std::string> params;
+} HttpRequest;
+
+} // namespace types
+} // namespace common
+} // namespace cb
+
+#endif
